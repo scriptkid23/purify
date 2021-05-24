de29ed1dd5648a67e57b413b8d5aadd1fdeb41ea
HoanDo <sieunhannhenkutesn@gmail.com>
2021-05-24 22:14:20 +0700

    Update realtime chart

--- src/App.js
+++ src/App.js
@@ -1,42 +1,134 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import {LineChart, Grid} from './components/Charts';
-import {Defs, LinearGradient, Stop} from 'react-native-svg';
+import {LineChart, Grid, Path, YAxis, XAxis} from './components/Charts';
+import {
+  Defs,
+  LinearGradient,
+  Stop,
+  ClipPath,
+  Rect,
+  Line,
+} from 'react-native-svg';
 
 const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80];
-
-export default function App() {
-  const Gradient = () => (
-    <Defs key={'gradient'}>
-      <LinearGradient id={'gradient'} x1={'0'} y={'0%'} x2={'100%'} y2={'0%'}>
-        <Stop offset={'0%'} stopColor={'rgb(134, 65, 244)'} />
-        <Stop offset={'100%'} stopColor={'rgb(66, 194, 244)'} />
-      </LinearGradient>
+const Clips = ({x, y, indexToClipFrom, height, bountry}) => {
+  return (
+    <Defs key={'clips'}>
+      <ClipPath id="clip-path-1">
+        <Rect x={'0'} y={'0'} width={x(indexToClipFrom) - 2} height={'100%'} />
+      </ClipPath>
+      <ClipPath id="clip-path-2">
+        <Rect
+          x={'0'}
+          y={'0'}
+          width={x(indexToClipFrom) - 2}
+          height={y(bountry)}
+        />
+      </ClipPath>
     </Defs>
   );
-
+};
+const RedDashedLine = ({y, bountry}) => (
+  <Line
+    x1="0"
+    y1={y(bountry)}
+    x2="100%"
+    y2={y(bountry)}
+    stroke="red"
+    strokeWidth="2"
+    strokeDasharray={[4, 4]}
+  />
+);
+const LineRealTime = ({line}) => (
+  <Path
+    key={'line-1'}
+    d={line}
+    stroke={'red'}
+    strokeWidth={2}
+    fill={'none'}
+    clipPath={'url(#clip-path-2)'}
+  />
+);
+const RealTimeLineGraph = props => {
+  const {yaxisData, indexToClipFrom, bountry, yMax} = props;
+  const contentInset = {top: 20, bottom: 20};
   return (
-    <View>
-      <LineChart
-        style={{height: 200}}
-        data={data}
-        contentInset={{top: 20, bottom: 20}}
-        svg={{
-          strokeWidth: 2,
-          stroke: 'url(#gradient)',
-        }}>
-        <Grid />
-        <Gradient />
-      </LineChart>
-      <Text>Click me</Text>
+    <View style={{flex: 1}}>
+      <View style={{height: 200, flexDirection: 'row'}}>
+        <YAxis
+          data={yaxisData}
+          contentInset={contentInset}
+          svg={{
+            fill: 'grey',
+            fontSize: 10,
+          }}
+          formatLabel={value => `${value}`}
+        />
+
+        <LineChart
+          style={{flex: 1, marginLeft: 16}}
+          yMin={0}
+          yMax={yMax}
+          data={yaxisData}
+          svg={{
+            stroke: 'green',
+            strokeWidth: 2,
+            clipPath: 'url(#clip-path-1)',
+          }}
+          contentInset={contentInset}>
+          <Grid />
+          <Clips indexToClipFrom={indexToClipFrom} bountry={bountry} />
+          <LineRealTime />
+          <RedDashedLine bountry={bountry} />
+        </LineChart>
+      </View>
     </View>
   );
+};
+
+class App extends React.Component {
+  max = 150;
+  bountry = 80;
+  timer;
+  constructor(props) {
+    super(props);
+    const yaxisData = Array.from(Array(this.max).keys()).map(() => {
+      return 0;
+    });
+
+    this.state = {yaxisData, counter: 0};
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      const {yaxisData, counter} = this.state;
+      if (yaxisData.length > this.max) {
+        clearInterval(this.timer);
+        return;
+      }
+      yaxisData[counter] = this.getRandomInt(this.max);
+      this.setState({yaxisData, counter: counter + 1});
+    }, 1000);
+  }
+
+  
+
+  getRandomInt = max => {
+    return Math.floor(Math.random() * Math.floor(this.max));
+  };
+
+  render() {
+    return (
+      <View style={{flex: 1, padding: 20}}>
+        <RealTimeLineGraph
+          yaxisData={this.state.yaxisData}
+          indexToClipFrom={this.state.counter - 1}
+          bountry={this.bountry}
+          yMax={this.max}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+export default App;
