25cd245889f61164b47004757f5aaee7b373a86a
HoanDo <sieunhannhenkutesn@gmail.com>
2021-06-07 20:38:23 +0700

    add package react-native-serialport

--- android/app/src/main/java/com/purify/MainApplication.java
+++ android/app/src/main/java/com/purify/MainApplication.java
@@ -3,6 +3,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.melihyarikkaya.rnserialport.RNSerialportPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
